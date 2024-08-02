a5b67058e06b15ef45322bfdf67786d0527a2bf6
blondelarnold <blondelarnold05@gmail.com>
2024-08-02 13:12:26 +0100

    image_Galery

--- image-gallery/image-gallery-example/script.js
+++ image-gallery/image-gallery-example/script.js
@@ -1,15 +1,41 @@
 var imageThumbs = document.getElementById("image-thumbs");
 var currentImage = document.getElementById("current-image");
+var prevBtn = document.getElementById("prev-btn");
+var nextBtn = document.getElementById("next-btn");
 
-for (var i = 1; i <= 10; i++) {
+var currentIndex = 0;
+var imageCount = 10;
+
+for (var i = 1; i <= imageCount; i++) {
   var thumb = document.createElement("img");
   thumb.src = "images/image" + i + ".jpg";
   thumb.alt = "Image " + i;
   thumb.classList.add("thumb");
   imageThumbs.appendChild(thumb);
   thumb.addEventListener(
-    "click", function() {
-      currentImage.src = this.src;
+    "click",
+    function() {
+      currentIndex = Array.from(imageThumbs.children).indexOf(this);
+      updateCurrentImage();
     }
   );
 }
+
+prevBtn.addEventListener("click", function() {
+  currentIndex = (currentIndex - 1 + imageCount) % imageCount;
+  updateCurrentImage();
+});
+
+nextBtn.addEventListener("click", function() {
+  currentIndex = (currentIndex + 1) % imageCount;
+  updateCurrentImage();
+});
+
+function updateCurrentImage() {
+  currentImage.src = "images/image" + (currentIndex + 1) + ".jpg";
+}
+
+setInterval(function() {
+  currentIndex = (currentIndex + 1) % imageCount;
+  updateCurrentImage();
+}, 5000);
